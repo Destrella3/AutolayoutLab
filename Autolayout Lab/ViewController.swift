ba1219482aad748fc1ee6629894bbe4021d014ec
Destrella3 <destrella385@gmail.com>
2018-11-14 14:25:11 -0500

    first commit

---
+++ Autolayout Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout Lab
+//
+//  Created by Diego Estrella III on 11/14/18.
+//  Copyright © 2018 Diego Estrella III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
